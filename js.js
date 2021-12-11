a9a4bd20e40afc90aca1c6a019dd92d27c598271
Jubn-yan <jubn@mail.uc.edu>
2021-12-11 22:33:23 +0800

    change the size of container

---
+++ js.js
@@ -0,0 +1,14 @@
+var contain = document.getElementById("container");
+var box = document.getElementById("playBox");
+var board = document.getElementsByClassName("board")[0];
+var ball = document.getElementsByClassName("ball")[0];
+var btn = document.getElementsByTagName("button")[0];
+var score = document.getElementsByClassName("score")[0];
+var board_left,board_top,ball_left,ball_top ;
+var interval,flag=1,sum=0; 
+
+
+var speedX = -1,speedY = -1;
+ 
+
+
